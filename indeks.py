07a3b457ca3d99f5b1582e9484e74debd4db3f86
kieriam <kieriamm@gmail.com>
2026-05-23 22:33:58 +0200

    indeksy

--- indeks.py
+++ indeks.py
@@ -1,56 +1,39 @@
-"""
-Zadanie 3 - Indeksowanie dokumentów
-
-Opis zadania:
-- Wejście:
-    * Pierwsza linia: liczba dokumentów do przetworzenia (n).
-    * Kolejne n linii: dokumenty (każdy dokument to wielowyrazowy ciąg znaków).
-    * Następna linia: liczba zapytań (m).
-    * Kolejne m linii: zapytania (każdy zapytanie to pojedynczy wyraz).
-- Wyjście:
-    * m linii, z których każda zawiera listę numerów dokumentów, w których wystąpił wyraz z zapytania.
-    * Każda lista jest posortowana według częstości wystąpienia zapytania w danym dokumencie (od największej do najmniejszej).
-    * W przypadku równych częstości, lista może być posortowana malejąco wg numeru dokumentu (opcjonalnie).
-    * Jeśli słowo nie wystąpiło w żadnym dokumencie, zwróć pustą listę.
-
-Przykładowe wejście:
-    3
-    Your care set up, do not pluck my care down.
-    My care is loss of care with old care done.
-    Your care is gain of care when new care is won.
-    2
-    care
-    is
-
-Przykładowe wyjście:
-    [1, 2, 0]
-    [2, 1]
-
-Wymagania:
-- Implementacja funkcji `index_documents(documents: list[str], queries: list[str]) -> list[list[int]]`.
-- Przetwarzanie tekstu – można użyć podziału na wyrazy, ignorując interpunkcję i wielkość liter.
-- Obliczenie liczby wystąpień danego wyrazu w każdym dokumencie.
-- Dla każdego zapytania, zwrócenie posortowanej listy indeksów dokumentów.
-"""
-
 
 def index_documents(documents: list[str], queries: list[str]) -> list[list[int]]:
     """
     Przetwarza dokumenty i zapytania, zwracając listy indeksów dokumentów,
     w których występuje zapytanie, posortowane według częstości wystąpienia
     danego wyrazu (malejąco), a w przypadku równych częstości - malejąco wg numeru dokumentu.
-
-    Args:
-        documents (list[str]): Lista dokumentów (każdy dokument to ciąg znaków).
-        queries (list[str]): Lista zapytań (każdy zapytanie to pojedynczy wyraz).
-
-    Returns:
-        list[list[int]]: Lista wyników dla kolejnych zapytań.
     """
-    ### TUTAJ PODAJ ROZWIĄZANIE ZADANIA
+    results = []
 
-    ### return [[]] - powinno być zmienione i zwrócić prawdziwy wynik (zgodny z oczekiwaniami)
-    return [[]]
+    for query in queries:
+
+        query = query.lower()
+
+        counts = []
+
+        for i, doc in enumerate(documents):
+
+            # usuwanie podstawowej interpunkcji
+            cleaned = doc.lower()
+
+            for znak in ".,!?;:":
+                cleaned = cleaned.replace(znak, "")
+
+            words = cleaned.split()
+
+            count = words.count(query)
+
+            if count > 0:
+                counts.append((i, count))
+
+        counts.sort(key=lambda x: (-x[1], x[0]))
+
+        results.append([doc_id for doc_id, _ in counts])
+
+    return results
+    
 
 
 # Przykładowe wywołanie:
